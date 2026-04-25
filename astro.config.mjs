991b6b48e22a52a8ed30cb3bbb87bc719c9cd0dd
Ajeet Yadav <yadav.ajeetsingh2020@gmail.com>
2026-04-24 23:03:39 -0500

    feat: add deploy workflow and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://vertexnova.github.io',
   integrations: [sitemap()]
 });
